79e87c8c2614a706bce9d02bd8e52e0295c82375
yanwen.wu2203 <yawen.wu2203@chancegiver.com>
2025-10-27 17:55:32 +0800

    菜单操作栏

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_os::init())
         .invoke_handler(tauri::generate_handler![
             greet,
             save_token,
--- src/layouts/components/header.vue
+++ src/layouts/components/header.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { getCurrentWindow } from '@tauri-apps/api/window';
 import {ref} from "vue";
+import SvgIcon from '@/components/svgIcon/index.vue';
+import { platform } from '@tauri-apps/plugin-os';
 
 const appWindow = ref() ;
 try {
@@ -8,55 +10,43 @@ try {
 } catch (error) {
   console.info('=====',error);
 }
+const osPlatform = platform();
+const isMax = ref(false);
 </script>
 
 <template>
   <div class="titlebar">
     <div data-tauri-drag-region></div>
-    <div class="controls">
-      <button id="titlebar-minimize" title="minimize" @click="appWindow.minimize()">
-        <!-- https://api.iconify.design/mdi:window-minimize.svg -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path fill="currentColor" d="M19 13H5v-2h14z" />
-        </svg>
-      </button>
-      <button id="titlebar-maximize" title="maximize" @click="appWindow.toggleMaximize()">
-        <!-- https://api.iconify.design/mdi:window-maximize.svg -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path fill="currentColor" d="M4 4h16v16H4zm2 4v10h12V8z" />
-        </svg>
-      </button>
-      <button id="titlebar-close" title="close" @click="appWindow.close()">
-        <!-- https://api.iconify.design/mdi:close.svg -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="currentColor"
-            d="M13.46 12L19 17.54V19h-1.46L12 13.46L6.46 19H5v-1.46L10.54 12L5 6.46V5h1.46L12 10.54L17.54 5H19v1.46z"
-          />
-        </svg>
-      </button>
+    <div class="mac-controls" v-if="osPlatform === 'macos'">
+      <div class="control-btn close-btn" @click="appWindow.close()">
+        <SvgIcon name="icon-cucha" size="8"></SvgIcon>
+      </div>
+      <div class="control-btn min-btn" @click="appWindow.minimize()">
+        <SvgIcon name="icon-zuixiaohua-mac" size="8"></SvgIcon>
+      </div>
+      <div class="control-btn max-btn" @click="appWindow.toggleMaximize();isMax = !isMax">
+        <SvgIcon name="icon-zuidahua-mac" size="8" v-if="!isMax"></SvgIcon>
+        <SvgIcon name="icon-huanyuan-mac" size="10" v-else></SvgIcon>
+      </div>
+    </div>
+    <div class="win-controls" v-else>
+      <div class="control-btn minimize" @click="appWindow.minimize()">
+        <SvgIcon name="icon-zuixiaohua" size="14"></SvgIcon>
+      </div>
+      <div class="control-btn maximize" @click="appWindow.toggleMaximize();isMax = !isMax">
+        <SvgIcon name="icon-zuidahua-windows" size="14" v-if="!isMax"></SvgIcon>
+        <SvgIcon name="icon-huanyuan-windows" size="14" v-else></SvgIcon>
+      </div>
+      <div class="control-btn close-btn" @click="appWindow.close()">
+        <SvgIcon name="icon-cha" size="14"></SvgIcon>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .titlebar {
-  height: 34px;
+  height: 28px;
   background: transparent;
   user-select: none;
   display: grid;
@@ -67,36 +57,72 @@ try {
   right: 0;
   z-index:9999999999;
 }
-.titlebar > .controls {
-  display: flex;
-}
-.titlebar button {
-  appearance: none;
-  padding: 0;
-  margin: 0;
-  border: none;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  width: 30px;
-  background-color: transparent;
-}
-.titlebar button:hover {
-  background: #5bbec3;
-}
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
+.titlebar {
+  .mac-controls {
+    position: absolute;
+    left: 10px;
+    top: 10px;
+    display: flex;
 
-  color: #0f0f0f;
-  background-color: #f6f6f6;
+    .svg-icon{
+      display: none;
+    }
 
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
+    &:hover{
+      .svg-icon{
+        display: block;
+      }
+    }
+
+    .control-btn{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 14px;
+      height: 14px;
+      margin-right: 6px;
+      border-radius: 50px;
+      border: 1px solid transparent;
+    }
+
+    .min-btn{
+      background: #febc2f;
+      border: 1px solid #e8a719;
+    }
+
+    .close-btn{
+      background: #ff5f57;
+      border: 1px solid #e8483f;
+    }
+
+    .max-btn{
+      background: #29cc42;
+      border: 1px solid #12b227;
+    }
+  }
+  .win-controls {
+    position: absolute;
+    right: 16px;
+    top: 6px;
+    display: flex;
+
+    .svg-icon{
+      color: $font-middle;
+    }
+
+    .control-btn{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 22px;
+      height: 22px;
+      margin-left: 12px;
+
+      &:hover{
+        background: $hover-color;
+      }
+    }
+  }
 }
+
 </style>
