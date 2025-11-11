7646eb04da52d0aac151b3d0d4d9cbf66eb85423
haitao <jaitao.li2303@chancegiver.com>
2025-11-11 09:08:57 +0800

    fix: dto 更新

--- src/api/ai/dto/Polish.ts
+++ src/api/ai/dto/Polish.ts
@@ -2,12 +2,27 @@ import { BaseInDto, BaseOutDto } from "@/api/BaseDto";
 
 export class PolishInDto extends BaseInDto {
     /**
+     * 简历ID
+     */
+    resumeId: string = '';
+    
+    /**
      * 需要优化的原文
      */
     text: string = '';
+    
+    /**
+     * 需要优化的字段名
+     */
+    fieldName: string = '';
+    
+    /**
+     * 最大优化长度
+     */
+    maxLength: number = 0;
 
     /**
-     * 优化模式（"polish", "expand", "simplify"）
+     * 优化模式（1-润色 2-扩展 3-简化 4-总结）
      */
     mode: string = '';
 
