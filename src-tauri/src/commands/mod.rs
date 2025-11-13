9a8a632cb0bad901010a687a9ef26cacd3973d90
yanwen.wu2203 <yawen.wu2203@chancegiver.com>
2025-11-13 12:01:54 +0800

    图片转base64命令

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,10 +1,12 @@
 mod auth;
 mod http;
+mod image;
 mod keyboard;
 mod sse;
 // mod keyboard;
 
 pub use auth::{save_token, get_token};
 pub use http::http_request;
+pub use image::image_to_base64;
 pub use sse::sse_request;
 // pub use keyboard::listen_save_shortcut;
