576e2fd4ea55c847c2802e1b0f57282f4768985d
Ming <>
2025-11-22 16:33:43 +0800

    build: 添加 code-inspector-plugin 和 prettier 依赖
    添加 code-inspector-plugin 用于代码调试，并集成 prettier 用于代码格式化

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,55 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { createSvgIconsPlugin }from 'vite-plugin-svg-icons';
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import {createSvgIconsPlugin} from 'vite-plugin-svg-icons';
 import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import {codeInspectorPlugin} from 'code-inspector-plugin';
 import path from 'path';
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [
-    vue(),
-    VueSetupExtend(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
-      symbolId: 'icon-[dir]-[name]'
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // additionalData: '@import "@/assets/styles/compute.scss"; @import "@/assets/styles/variable.scss"; '
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      // 设置路径
-      '@': path.resolve(__dirname, './src')
-    },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-  },
-  clearScreen: false,
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
+    plugins: [
+        vue(),
+        VueSetupExtend(),
+        codeInspectorPlugin({
+            bundler: 'vite',
+            editor: 'code',
+            showSwitch: true
+        }),
+        createSvgIconsPlugin({
+            iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
+            symbolId: 'icon-[dir]-[name]'
+        })
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // additionalData: '@import "@/assets/styles/compute.scss"; @import "@/assets/styles/variable.scss"; '
+            }
         }
-      : undefined,
-    watch: {
-      ignored: ["**/src-tauri/**"],
     },
-  },
+    resolve: {
+        alias: {
+            // 设置路径
+            '@': path.resolve(__dirname, './src')
+        },
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    },
+    clearScreen: false,
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                  protocol: 'ws',
+                  host,
+                  port: 1421
+              }
+            : undefined,
+        watch: {
+            ignored: ['**/src-tauri/**']
+        }
+    }
 }));
