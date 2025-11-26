0ab63a3fd21512901a9113ebee823293f7ce7c03
runyang <runyang.an2321@chancegiver.com>
2025-11-26 11:47:38 +0800

    更新职位相关dto

--- src/api/job/dto/CreateJobTask.ts
+++ src/api/job/dto/CreateJobTask.ts
@@ -40,47 +40,7 @@ export class CreateJobTaskInDto extends BaseInDto {
 
 export class CreateJobTaskOutDto extends BaseOutDto {
     /**
-     * 主键
+     * 创建成功返回的数量
      */
-    uuid: string = '';
-
-    /**
-     * 任务名称
-     */
-    taskName: string = '';
-
-    /**
-     * 求职岗位
-     */
-    jobTitle: string = '';
-
-    /**
-     * 期望城市区域代码
-     */
-    cityCode: string = '';
-
-    /**
-     * 期望城市名称
-     */
-    cityName: string = '';
-
-    /**
-     * 工作经验
-     */
-    experience: string = '';
-
-    /**
-     * 关联的简历UUID
-     */
-    resumeUuid: string = '';
-
-    /**
-     * 任务状态 (0: 运行中, 1: 未激活, 2: 已归档)
-     */
-    status: number = 0;
-
-    /**
-     * 是否为默认任务
-     */
-    isDefault: boolean = false;
+    value: number = 0;
 }
