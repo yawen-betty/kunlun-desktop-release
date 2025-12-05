a2d69459d0b3be2137027512889b86b9e6a83943
yanwen.wu2203 <yawen.wu2203@chancegiver.com>
2025-12-05 19:40:46 +0800

    打包修改

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -20,14 +20,19 @@ use ai::AIManager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_os::init())
-        #[cfg(desktop)]
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_os::init());
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
+    }
+
+    builder
         .manage(Mutex::new(McpManager::new()))
         .invoke_handler(tauri::generate_handler![
             greet,
