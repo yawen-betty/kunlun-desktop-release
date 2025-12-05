d2717dd2ddd4e9b64be566b83e2629e32ec68dbb
yanwen.wu2203 <yawen.wu2203@chancegiver.com>
2025-12-05 19:28:14 +0800

    打包修改

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -26,6 +26,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_os::init())
+        #[cfg(desktop)]
         .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(Mutex::new(McpManager::new()))
         .invoke_handler(tauri::generate_handler![
